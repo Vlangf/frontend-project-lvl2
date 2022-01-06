43effef4c0e180d93b224b766300773d866c2bfb
valentin.gorbachev <valentin.gorbachev@aliexpress.com>
2022-01-06 21:07:03 +0300

    added test

---
+++ __tests__/compareFlatJson.test.js
@@ -0,0 +1,20 @@
+
+import { readFileSync } from 'fs'; 
+import {dirname, join} from 'path';
+import genDiff from '../src/genDiff';
+import { fileURLToPath } from 'url';
+import parseFile from '../src/parseFile';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const fixturesPath = join(__dirname, '__fixtures__')
+
+test('Compare two flat jsons', () =>{
+    const firstJson = join(fixturesPath, 'fileOne.json');
+    const secondJson = join(fixturesPath, 'fileTwo.json');
+    const expectedResult = readFileSync(join(fixturesPath, 'result.txt'), 'utf-8');
+    const actualResult = genDiff(parseFile(firstJson), parseFile(secondJson));
+    
+    expect(actualResult).toMatch(expectedResult);
+})
